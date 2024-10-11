658de065596ab1d57e8b0dbe3ef526cba2e935c0
Miguel Bonilla <miguelbonilla1886@gmail.com>
2024-10-11 15:47:38 -0400

    HeroSection fix

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,8 +9,8 @@ export default function HeroSection() {
   return (
     <section id="HeroSection" className="relative w-full min-h-screen overflow-hidden bg-gradient-to-r from-gray-800 via-gray-900 to-black">
       <AnimatedBackground />
-      <div className="relative z-20 flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen px-4 lg:px-36 py-12">
-        <div className="w-full lg:w-1/2 text-center lg:text-left">
+      <div className="relative z-20 flex flex-col-reverse lg:flex-row items-center justify-center min-h-screen px-4 sm:px-6 md:px-8 lg:px-16 xl:px-24 2xl:px-36 py-12">
+        <div className="w-full lg:w-1/2 text-center lg:text-left lg:pr-4 xl:pr-8">
           <h1 className="text-tertiary text-2xl lg:text-5xl font-bold mt-4 md:mt-8 lg:mt-0">
             <span className="bg-gradient-to-r from-blue-600 via-purple-600 to-pink-700 text-transparent bg-clip-text">Hey There! </span>
           </h1>
@@ -18,7 +18,7 @@ export default function HeroSection() {
             <span className="bg-gradient-to-r from-blue-600 via-purple-600 to-pink-700 text-transparent bg-clip-text">I&apos;m Miguel Bonilla</span>
           </h2>
           <h2 className="text-tertiary text-2xl lg:text-5xl font-bold">
-            <span className="bg-gradient-to-r from-yellow-400 via-orange-500 to-purple-600 inline-block text-transparent bg-clip-text">Full Stack developer</span>
+            <span className="bg-gradient-to-r from-yellow-400 via-orange-500 to-purple-600 inline-block text-transparent bg-clip-text  whitespace-nowrap">Full Stack developer</span>
           </h2>
           <div className="flex justify-center lg:justify-start items-center mt-5 lg:ml-2 lg:mt-10">
             <Link href="https://www.linkedin.com/in/miguel-bonilla-4b7438285/" target="_blank" rel="noopener noreferrer" className="text-white transition-transform transform hover:scale-105 mr-6">
@@ -32,13 +32,12 @@ export default function HeroSection() {
             </Link>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end mb-8 ">
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end mb-8 lg:mb-0">
           <div className="relative w-[40vh] h-[40vh] sm:w-[40vh] sm:h-[40vh] md:w-[40vh] md:h-[40vh] lg:w-[45vh] lg:h-[45vh]">
             <Image
               src="/images/fotoPortafolio5.png"
               alt="Miguel Bonilla"
               layout="fill"
-            
               quality={100}
               className="rounded-full border-4 border-orange-700 border-opacity-90 shadow-[0px_0px_20px_10px_rgba(234,88,12,0.6)]"
             />
