62c40d16520c5aba254eeb433c02390a9d016e83
Miguel Bonilla <miguelbonilla1886@gmail.com>
2024-10-11 23:37:07 -0400

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head><meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`${inter.className} relative`}>
         
         <div className="relative z-10">
