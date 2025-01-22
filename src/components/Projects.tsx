f844e2ef5f35782c061a2a2c3857deac9e6c35d1
Miguel Bonilla <miguelbonilla1886@gmail.com>
2025-01-22 00:10:10 -0400

    a

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -37,7 +37,7 @@ export default function Projects() {
     },
     {
       name: 'Discord Bot',
-      url: 'https://discord.gg/HqwVj4zs',
+      url: 'https://discord.gg/e52y2p38',
       imageUrl: '/images/dcapp.jpeg',
       description: 'The Discord Bot provides real-time cryptocurrency quotes, using Discord.js, Node.js, and the CoinMarketCap API.',
       gifUrl: '/images/bot-testing.gif',
