87fe07ac7ab7c22bbc50c0c8ffb63052451e7237
Miguel Bonilla <miguelbonilla1886@gmail.com>
2025-02-20 01:46:20 -0400

    1

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHtml5, faCss3Alt, faJsSquare, faReact, faBootstrap } from '@fortawesome/free-brands-svg-icons'
 import { DiNodejs } from "react-icons/di"
-import { SiNestjs, SiTypescript } from "react-icons/si"
+// import { SiNestjs, SiTypescript } from "react-icons/si"
 import { BiLogoPostgresql, BiLogoTailwindCss } from "react-icons/bi"
 import { GrMysql } from "react-icons/gr"
 import { SiPhp } from 'react-icons/si'
